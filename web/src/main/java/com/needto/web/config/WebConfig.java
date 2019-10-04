43e9cd08564e68e90111039ca93a1a48368f1b2f
wangkai <962471630@qq.com>
2019-10-04 22:39:07 +0800

    清除冗余代码

--- web/src/main/java/com/needto/web/config/WebConfig.java
+++ web/src/main/java/com/needto/web/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.needto.web.config;
 import com.needto.web.inter.ClientIntercepter;
 import com.needto.web.session.HeaderCookieHttpSessionIdResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.converter.HttpMessageConverter;
 
 /**
  * @author Administrator
@@ -43,5 +45,14 @@ public class WebConfig {
     public HeaderCookieHttpSessionIdResolver headerCookieHttpSessionIdResolver(){
         String clientKey = environment.getProperty("client.key", HeaderCookieHttpSessionIdResolver.TOKEN_KEY);
         return new HeaderCookieHttpSessionIdResolver(clientKey);
+    }
+
+    /**
+     * json转换器
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        return new HttpMessageConverters((HttpMessageConverter<?>) MessageConvert.getHttpMessageConverters());
     }
 }
